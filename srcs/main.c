28e025ef88395920814fb2f1dce9a8a516031cd5
maxime <maxime.cauchy93@gmail.com>
2022-06-21 11:44:35 +0200

    push auto

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,23 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 17:39:58 by mcauchy           #+#    #+#             */
-/*   Updated: 2022/06/20 22:36:08 by mcauchy          ###   ########.fr       */
+/*   Updated: 2022/06/21 11:26:13 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+//Parcourir la liste chainee et remplir une autre liste chaine qui contient les commandes dans un char** sans pipe ni chevron
+//+tableau de pid dans cette liste qui sera malloc au nombre de commande
+//+tableau de pipe malloc a (nb commande - 1) * 2
+//il faut pipe chaque pipe adjacente donc pipe(params->fd + i * 2);
+//Parcourir cette nouvelle liste chainee et fork a chaque fois,
+//si c la premiere commande
+//on dup les bon infile + outfile
+//si c la derniere commande on pipe les bon fd + outfile
+//si c des commandes entre les deux on pipe les bon fd + outfile
+//close certains fd
+//ensuite execve
 
 void	ft_print_tab(char **tab)
 {
