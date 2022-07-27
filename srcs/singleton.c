8916ce51f1c624ed3a5faa756e2c0bd1280d98d6
maxime <maxime.cauchy93@gmail.com>
2022-07-27 12:19:08 +0200

    added a singleton for the data struct

--- srcs/singleton.c
+++ srcs/singleton.c
@@ -6,7 +6,7 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 18:26:30 by mcauchy           #+#    #+#             */
-/*   Updated: 2022/06/21 18:27:45 by mcauchy          ###   ########.fr       */
+/*   Updated: 2022/07/26 13:26:05 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,17 @@ t_list	*_lst(void)
 		ft_bzero(&lst, sizeof(t_list));
 	}
 	return (&lst);
+}
+
+t_data	*_data(void)
+{
+	static t_data	data;
+	static int		init = 0;
+
+	if (init == 0)
+	{
+		init = 1;
+		ft_bzero(&data, sizeof(t_data));
+	}
+	return (&data);
 }
