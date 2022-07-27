bc7eb8b4fee129407fde4a37985fb11bb38dd2d4
maxime <maxime.cauchy93@gmail.com>
2022-07-27 12:16:54 +0200

    init *PIDs

--- srcs/init.c
+++ srcs/init.c
@@ -6,15 +6,21 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 15:21:50 by mcauchy           #+#    #+#             */
-/*   Updated: 2022/06/21 16:28:45 by mcauchy          ###   ########.fr       */
+/*   Updated: 2022/07/26 13:32:04 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	init_exec(void)
+void	init_pid(void)
 {
-	t_list	*lst;
+	t_data	*temp;
+	t_list	*temp2;
 
-	lst = _lst();
+	temp = _data();
+	temp2 = _lst();
+	temp->nb_cmd = ft_lstsize(temp2);
+	temp->pid = malloc(sizeof(int) * (temp->nb_cmd));
+	if (temp->pid == NULL)
+		hasta_la_vista();
 }
