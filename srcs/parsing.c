fa08a9307f74f853e60293231949dd6e48eed263
maxime <maxime.cauchy93@gmail.com>
2022-09-03 10:41:10 +0200

    "l"s

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 18:38:03 by mcauchy           #+#    #+#             */
-/*   Updated: 2022/09/02 18:51:00 by mcauchy          ###   ########.fr       */
+/*   Updated: 2022/09/03 10:32:56 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	parsing(char *cmd)
 
 	data = _data();
 	cmd = refacto_token_space(cmd);
-	token = ft_split_parsing(cmd, ' ');
+	token = ft_split(cmd, ' ');
 	if (!cmd || !token)
 		hasta_la_vista(0);
 	token = expand(token);
